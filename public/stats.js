4c6c91b8271a070292f3e2a3e44ab04cac8b69c4
Kailu <kayalur@purdue.edu>
2018-04-24 03:17:53 -0400

    altered some formatting with regard to the stats.

--- public/stats.js
+++ public/stats.js
@@ -15,15 +15,20 @@ function gotStats(){
 		console.log("tempc2 value is " + tempc2);
 		var total = tempc2 + tempc1;
 
-		var perc1 = tempc1/total;
-		perc1 = Math.round(perc1*100)/100;
-		perc1 *= 100;
-		console.log("perc1 val is: " + perc1);
-		var perc2 = tempc2/total;
-		perc2 = Math.round(perc2*100)/100;
-		perc2 *= 100;
-		console.log("perc2 val is: " + perc2);
-
+		if(total == 0){
+			perc1 = 0;
+			perc2 = 0;
+		}
+		else{
+			var perc1 = tempc1/total;
+			perc1 = Math.round(perc1*100)/100;
+			perc1 *= 100;
+			console.log("perc1 val is: " + perc1);
+			var perc2 = tempc2/total;
+			perc2 = Math.round(perc2*100)/100;
+			perc2 *= 100;
+			console.log("perc2 val is: " + perc2);
+		}
 		content += '<tr>';
         content += '<td>' + newPost.question + '</td>'; //column1
         content += '<td style="text-align: center;">' + perc1 + " %" + '</td>';//column2
